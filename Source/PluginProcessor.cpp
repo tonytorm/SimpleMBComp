08c230151e2f7cb47d8de7a3448918aec5a652b0
Giulio Iacomino <giulio.iacomino92@gmail.com>
2021-12-05 15:21:16 +0000

    Params Namespace Implementation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -24,20 +24,39 @@ SimpleMbCompAudioProcessor::SimpleMbCompAudioProcessor()
                        )
 #endif
 {
-    compressor.attack = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Attack"));
-    jassert(compressor.attack != nullptr);
+    using namespace Params;
+    const auto& params = GetParams();
     
-    compressor.release = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Release"));
-    jassert(compressor.release != nullptr);
+    auto floatHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    {
+        param = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter(params.at(paramName)));
+        jassert(param != nullptr);
+        
+    };
     
-    compressor.threshold = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter("Threshold"));
-    jassert(compressor.threshold != nullptr);
+    floatHelper(compressor.attack, Names::Attack_Low_Band);
+    floatHelper(compressor.release, Names::Release_Low_Band);
+    floatHelper(compressor.threshold, Names::Threshold_Low_band);
     
-    compressor.ratio = dynamic_cast<juce::AudioParameterChoice*>(apvts.getParameter("Ratio"));
-    jassert(compressor.ratio != nullptr);
+    auto choiceHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+       {
+           param = dynamic_cast<juce::AudioParameterChoice*>(apvts.getParameter(params.at(paramName)));
+           jassert(param != nullptr);
+           
+       };
     
-    compressor.bypassed = dynamic_cast<juce::AudioParameterBool*>(apvts.getParameter("Bypassed"));
-    jassert(compressor.bypassed != nullptr);
+    choiceHelper(compressor.ratio, Names::Ratio_Low_Band);
+    
+    auto boolHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    {
+        param = dynamic_cast<juce::AudioParameterBool*>(apvts.getParameter(params.at(paramName)));
+        jassert(param != nullptr);
+        
+    };
+    
+    boolHelper(compressor.bypassed, Names::Bypassed_Low_Band);
+    
+    
        
 }
 
@@ -209,21 +228,25 @@ juce::AudioProcessorValueTreeState::ParameterLayout SimpleMbCompAudioProcessor::
     APVTS::ParameterLayout layout;
     
     using namespace juce;
+    using namespace Params;
     
-    layout.add(std::make_unique<AudioParameterFloat>("Threshold",
-                                                     "Threshold",
+    
+    const auto& params = GetParams();
+    
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Threshold_Low_band),
+                                                     params.at(Names::Threshold_Low_band),
                                                      NormalisableRange<float>(-60, 12, 1, 1),
                                                      0));
     
     auto attackReleaseRange = NormalisableRange<float>(5, 500, 1, 1);
     
-    layout.add(std::make_unique<AudioParameterFloat>("Attack",
-                                                     "Attack",
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Attack_Low_Band),
+                                                     params.at(Names::Attack_Low_Band),
                                                      attackReleaseRange,
                                                      50));
     
-    layout.add(std::make_unique<AudioParameterFloat>("Release",
-                                                     "Release",
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Release_Low_Band),
+                                                     params.at(Names::Release_Low_Band),
                                                      attackReleaseRange,
                                                      250));
     auto choices = std::vector<double> {1, 1.5, 2, 3, 4, 5, 6, 7, 8, 10, 15, 20, 50, 100};
@@ -232,9 +255,14 @@ juce::AudioProcessorValueTreeState::ParameterLayout SimpleMbCompAudioProcessor::
     {
         sa.add( String(choice, 1));
     }
-    layout.add(std::make_unique<AudioParameterChoice>("Ratio", "Ratio", sa, 3));
+    layout.add(std::make_unique<AudioParameterChoice>(params.at(Names::Ratio_Low_Band),
+                                                      params.at(Names::Ratio_Low_Band),
+                                                      sa,
+                                                      3));
     
-    layout.add(std::make_unique<AudioParameterBool>("Bypassed", "Bypassed", false));
+    layout.add(std::make_unique<AudioParameterBool>(params.at(Names::Bypassed_Low_Band),
+                                                    params.at(Names::Bypassed_Low_Band),
+                                                    false));
     
     return layout;
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,6 +10,61 @@
 
 #include <JuceHeader.h>
 
+namespace Params
+{
+enum Names
+{
+    Low_Mid_Crossover_Freq,
+    Mid_High_Crossover_Freq,
+    
+    Threshold_Low_band,
+    Threshold_Mid_band,
+    Threshold_High_band,
+    
+    Attack_Low_Band,
+    Attack_Mid_Band,
+    Attack_High_Band,
+    
+    Release_Low_Band,
+    Release_Mid_Band,
+    Release_High_Band,
+    
+    Ratio_Low_Band,
+    Ratio_Mid_Band,
+    Ratio_High_Band,
+    
+    Bypassed_Low_Band,
+    Bypassed_Mid_Band,
+    Bypassed_High_Band,
+};
+inline const std::map<Names,juce::String>& GetParams()
+{
+    static std::map<Names,juce::String> params =
+    {
+        {Low_Mid_Crossover_Freq, "Low-Mid Crossover Freq"},
+        {Mid_High_Crossover_Freq, "Mid-High Crossover Freq"},
+        {Threshold_Low_band, "Threshold Low Band"},
+        {Threshold_Mid_band, "Threshold Mid Band"},
+        {Threshold_High_band, "Threshold High Band"},
+        {Attack_Low_Band, "Attack Low Band"},
+        {Attack_Mid_Band, "Attack Mid Band"},
+        {Attack_High_Band, "Attack High Band"},
+        {Release_Low_Band, "Release Low Band"},
+        {Release_Mid_Band, "Release Mid Band"},
+        {Release_High_Band, "Release High Band"},
+        {Ratio_Low_Band, "Ratio Low Band"},
+        {Ratio_Mid_Band, "Ratio Mid Band"},
+        {Ratio_High_Band, "Ratio High Band"},
+        {Bypassed_Low_Band, "Bypassed Low Band"},
+        {Bypassed_Mid_Band, "Bypassed Mid Band"},
+        {Bypassed_High_Band, "Bypassed High Band"},
+    };
+    
+    return params;
+}
+}
+
+
 struct CompressorBand
 {
 public:
